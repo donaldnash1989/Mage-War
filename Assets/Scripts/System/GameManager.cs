361d6b4c5569891317f0a6972970a05a28afd56e
Donald E Nash <donaldnash1989@gmail.com>
2023-04-01 08:39:14 -0400

    Update GameManager.cs
    simple scene load

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public void Awake()
     {
         Application.targetFrameRate = 60;
+    }
+    private void Start()
+    {
+        SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
     }
 }
